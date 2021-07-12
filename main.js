dc9f5496168f016e5579506624751edfc6e1f927
Alex <alexs123655@gmail.com>
2021-07-12 17:08:56 -0300

    feat: toggle links created

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+/* Toggle menu */
 const nav = document.querySelector('#header nav');
-
 const toggle = document.querySelectorAll('nav .toggle');
 
 for(const element of toggle) {
     element.addEventListener('click', () => {
         nav.classList.toggle('show');
     })
+};
+
+/* On select options */
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+    link.addEventListener('click', () => {
+        nav.classList.remove('show');
+    });
 };
