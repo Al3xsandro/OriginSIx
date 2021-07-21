870808e6622c73bf1a3036e6c211300b89ae6960
Alex <alexs123655@gmail.com>
2021-07-21 20:43:15 -0300

    feat: scrollreveal.js

--- main.js
+++ main.js
@@ -38,3 +38,21 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
 });
+
+/* Scroll reveal */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+});
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links
+    `,
+    { interval: 100 }
+);
