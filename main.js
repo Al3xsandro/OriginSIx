881ae3c57bc3620653a3f5dd7cbab8d7a24690b3
Alex <alexs123655@gmail.com>
2021-07-23 21:26:12 -0300

    feat: backToTop button

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ for(const link of links) {
 };
 
 /* On scroll */
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header');
+    const navHeight = header.offsetHeight;
+    
     if(window.scrollY >= navHeight){
         header.classList.add('scroll');     
     } else {
         header.classList.remove('scroll');
     };
-});
+}
 
 /* Testimonials carousel swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -53,6 +53,22 @@ scrollReveal.reveal(
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials
     #contact .text, #contact .links
-    `,
+    footer .brand, footer .social`,
     { interval: 100 }
 );
+
+/* Back to top */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top');
+    
+    if(window.scrollY >= 560) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    };
+}
+
+window.addEventListener('scroll', () => {
+    backToTop();
+    changeHeaderWhenScroll();
+});
